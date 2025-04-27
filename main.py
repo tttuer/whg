170b90b760a4336a2aca4c95b01a88eb3e6ceb6e
tttuer <tttuer1@naver.com>
2025-04-27 12:01:06 +0900

    안쓰는것 정리

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from seleniumwire import webdriver
+from seleniumwire import webdriver  # type: ignore
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import time
 import json
 import csv
 import gzip
